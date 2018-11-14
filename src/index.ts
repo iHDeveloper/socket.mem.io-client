235a800b99f0132bfe603fca36784d444031ff19
iHDeveloper <hamzadevelop@gmail.com>
2018-11-14 19:35:00 +0300

    Add client class with basics and client options for customize the connect options

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import * as SocketIOClient from "socket.io-client";
+
+export class Client {
+    private options: ClientOptions;
+    private client: SocketIOClient.Socket;
+
+    constructor(options?: ClientOptions) {
+        if (options === undefined) {
+            this.options = new ClientOptions();
+        } else {
+            this.options = options;
+        }
+        const uri: string = `${this.options.https ? "https" : "http"}://${this.options.host}:${this.options.port}`;
+        this.client = SocketIOClient.default(uri);
+    }
+
+}
+
+export class ClientOptions {
+    public https: boolean = false;
+    public host: string = "localhost";
+    public port: number = 2030;
+}
